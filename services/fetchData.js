53d961dad355ad3c12a152924d198620b655c9f2
DiegoSantelices10 <diego.santelices@davinci.edu.ar>
2023-02-19 01:32:01 -0300

    update variables de entorno

--- services/fetchData.js
+++ services/fetchData.js
@@ -1,5 +1,5 @@
 export async function getProducts() {
-    const res = await fetch("http://localhost:3000/api/items")
-    const products = await res.json()
-    return products
+	const res = await fetch(process.env.API_URL + '/api/items');
+	const products = await res.json();
+	return products;
 }
