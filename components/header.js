dd3d3c9a0b3a84b6ccbefa856550fb4795fb7cbf
DiegoSantelices10 <diego.santelices@davinci.edu.ar>
2023-04-08 12:28:27 -0300

    realizando front de info de productos

--- components/header.js
+++ components/header.js
@@ -32,55 +32,77 @@ function Header() {
 					</div>
 
 					{/* Site navigation */}
-					<nav className="flex flex-grow font-nunito">
-						<ul className="flex flex-grow justify-end gap-4 flex-wrap items-center">
-							<li>
-								<Link href={"/signin"}>
+
+					<nav className="flex flex-grow font-nunito justify-end">
+						<button
+							data-collapse-toggle="navbar-default"
+							type="button"
+							className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+							aria-controls="navbar-default"
+							aria-expanded="false"
+						>
+							<span className="sr-only">Open main menu</span>
+							<svg
+								className="w-6 h-6"
+								aria-hidden="true"
+								fill="currentColor"
+								viewBox="0 0 20 20"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+									clip-rule="evenodd"
+								></path>
+							</svg>
+						</button>
+						<div className="hidden w-full md:block md:w-auto" id="navbar-default">
+							<ul className="flex flex-grow justify-end gap-4 flex-wrap items-center">
+								<li>
 									<a
+										href="#"
 										className={`${
 											!top ? "text-gray-800" : "text-white"
-										} font-medium   p-2 flex items-center transition duration-150 ease-in-out`}
+										} font-medium   p-2 block items-center transition duration-150 ease-in-out`}
+										aria-current="page"
 									>
 										Pizzas
 									</a>
-								</Link>
-							</li>
-							<li>
-								<Link href={"/signin"}>
+								</li>
+								<li>
 									<a
+										href="#"
 										className={`${
 											!top ? "text-gray-800" : "text-white"
-										} font-medium   p-2 flex items-center transition duration-150 ease-in-out`}
+										} font-medium   p-2 block items-center transition duration-150 ease-in-out`}
 									>
 										Empanadas
 									</a>
-								</Link>
-							</li>
-							<li>
-								<Link href={"/signin"}>
+								</li>
+								<li>
 									<a
+										href="#"
 										className={`${
 											!top ? "text-gray-800" : "text-white"
-										} font-medium   p-2 flex items-center transition duration-150 ease-in-out`}
+										} block font-medium   p-2  items-center transition duration-150 ease-in-out`}
 									>
 										Promociones
 									</a>
-								</Link>
-							</li>
-
-							<div
-								className={`p-1 rounded-md ${
-									!top && "bg-gray-900 text-white"
-								} bg-white w-auto px-2 hover:bg-red-500 hover:text-gray-50  hover:-translate-y-1 
+								</li>
+								<div
+									className={`p-1 rounded-md ${
+										!top && "bg-gray-900 text-white"
+									} bg-white w-auto px-2 hover:bg-red-500 hover:text-gray-50  hover:-translate-y-1 
 											transition-all duration-500`}
-							>
-								<Link href={"/order/login"}>
-									<a className="flex justify-around items-center">
-										<p className="  p-1 font-nunito font-bold">Hace tu pedido</p>
-									</a>
-								</Link>
-							</div>
-						</ul>
+								>
+									<Link href={"/order/login"}>
+										<a className="flex justify-around items-center">
+											<p className="  p-1 font-nunito font-bold">Hace tu pedido</p>
+										</a>
+									</Link>
+								</div>
+							</ul>
+						</div>
 					</nav>
 				</div>
 			</div>
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,27 @@
 import React from "react";
 import Header from "components/header";
 import HeroHome from "components/heroHome";
-import FeaturesHome from "components/featuresHome";
-import FeaturesBlocks from "components/featuresBlocks";
-import Testimonials from "components/testimonials";
-import Newsletter from "components/newSletter";
-import Footer from "components/footer";
+// import FeaturesHome from "components/featuresHome";
+// import FeaturesBlocks from "components/featuresBlocks";
+// import Testimonials from "components/testimonials";
+// import Newsletter from "components/newSletter";
+// import Footer from "components/footer";
 
 export default function index() {
 	return (
 		<div className="flex flex-col min-h-screen overflow-hidden bg-slate-900">
 			<Header />
-			<main className="flex-grow">
+			<main className="flex-grow bg-slate-900">
 				{/*  Page sections */}
 				<HeroHome />
-				<FeaturesHome />
+				{/* <FeaturesHome />
 				<FeaturesBlocks />
 				<Testimonials />
-				<Newsletter />
+				<Newsletter /> */}
 			</main>
 
 			{/*  Site footer */}
-			<Footer />
+			{/* <Footer /> */}
 		</div>
 	);
 }
