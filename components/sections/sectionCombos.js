c5a0940695b32ed52709a12a8271c362a89be8ca
DiegoSantelices10 <diego.santelices@davinci.edu.ar>
2023-06-29 23:29:34 -0300

    fix imagenes y build css

--- components/sections/sectionCombos.js
+++ components/sections/sectionCombos.js
@@ -46,7 +46,7 @@ export default function sectionCombos() {
         <div className="flex justify-center h-28 items-center w-full gap-10">
           <div className="text-center h-28 w-auto flex flex-col justify-between">
             <div className="h-24 flex justify-center items-center">
-              <Image src={"/images/empanadaBlanca.png"} width={90} height={75} alt="empanada" />
+              <Image src={"/images/empanadaBlanca.webp"} width={90} height={75} alt="empanada" />
             </div>
           </div>
           <div className="text-center h-28 w-auto flex flex-col justify-between ">
@@ -56,7 +56,7 @@ export default function sectionCombos() {
           </div>
           <div className="text-center h-28 w-auto flex flex-col justify-between ">
             <div className="h-24  flex justify-center items-center ">
-              <Image src={"/images/canastitaBlanca.png"} width={90} height={90} alt="canastita" />
+              <Image src={"/images/canastitaBlanca.webp"} width={90} height={90} alt="canastita" />
             </div>
           </div>
         </div>
