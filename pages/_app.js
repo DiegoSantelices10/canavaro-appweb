191b04b42022dd511cdf94a176f4479284bbfe63
DiegoSantelices10 <diego.santelices@davinci.edu.ar>
2023-07-03 00:58:19 -0300

    fix fonts

--- pages/_app.js
+++ pages/_app.js
@@ -2,36 +2,38 @@
 import { Provider } from "react-redux";
 import { wrapper } from "store/app/store";
 import { AnimatePresence, motion } from "framer-motion";
+import "typeface-nunito";
+
 import "styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, router, ...rest }) {
-	const { store, props } = wrapper.useWrappedStore(rest);
+  const { store, props } = wrapper.useWrappedStore(rest);
 
-	return (
-		<AnimatePresence>
-			<motion.div
-				key={router.route}
-				initial="initialState"
-				animate="animateState"
-				exit="exitState"
-				transition={{
-					duration: 1.25,
-				}}
-				variants={{
-					initialState: {
-						opacity: 0,
-					},
-					animateState: {
-						opacity: 2,
-					},
-				}}
-			>
-				<Provider store={store}>
-					<Component {...props.pageProps} />
-				</Provider>
-			</motion.div>
-		</AnimatePresence>
-	);
+  return (
+    <AnimatePresence>
+      <motion.div
+        key={router.route}
+        initial="initialState"
+        animate="animateState"
+        exit="exitState"
+        transition={{
+          duration: 1.25,
+        }}
+        variants={{
+          initialState: {
+            opacity: 0,
+          },
+          animateState: {
+            opacity: 2,
+          },
+        }}
+      >
+        <Provider store={store}>
+          <Component {...props.pageProps} />
+        </Provider>
+      </motion.div>
+    </AnimatePresence>
+  );
 }
 export default MyApp;
