8dd83797e57538164d0a5d0e61e46925bb3d9c96
DiegoSantelices10 <santelices.diego88@gmail.com>
2023-07-13 23:48:36 -0300

    agregar ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'canavaro-appweb',
+      script: 'node_modules/next/dist/bin/next',
+      interpreter: 'node',
+      interpreter_args: '17.9.1',
+      // otras opciones de configuración...
+    }
+  ]
+};
