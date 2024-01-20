0305bf9cc91f7ded4b6412f6e4974f9b33934b4d
DiegoSantelices10 <santelices.diego88@gmail.com>
2024-01-20 17:14:57 -0300

    fix button

--- components/header.js
+++ components/header.js
@@ -1,8 +1,6 @@
 import Link from "next/link";
-import { useSelector } from "react-redux";
 
 function Header() {
-  const { deliveryButton } = useSelector(state => state.setting);
 
   return (
     <header
@@ -11,19 +9,17 @@ function Header() {
       <div className="w-full sm:w-full md:w-full lg:w-11/12 mx-auto  sm:px-6">
         <div className="flex items-center  justify-center px-3 pt-5">
           <nav className="flex font-poppins ">
-            {deliveryButton.available && (
-              <div className="w-full flex items-center">
-                <Link href={"/welcomeLogo"}>
-                  <a
-                    className={`p-2 px-4 rounded-md font-semibold  font-poppins  text-lg  mx-auto 
+            <div className="w-full flex items-center">
+              <Link href={"/welcomeLogo"}>
+                <a
+                  className={`p-2 px-4 rounded-md font-semibold  font-poppins  text-lg  mx-auto 
                           hover:bg-black hover:text-white bg-white  text-gray-900   
                           hover:-translate-y-1 transition-all duration-500`}
-                  >
-                    Hac&eacute; tu pedido
-                  </a>
-                </Link>
-              </div>
-            )}
+                >
+                  Hac&eacute; tu pedido
+                </a>
+              </Link>
+            </div>
           </nav>
         </div>
       </div>
