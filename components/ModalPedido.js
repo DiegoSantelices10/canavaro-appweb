8b4fad017b93ccd644f1f9432adf7be6cd10d9a5
DiegoSantelices10 <santelices.diego88@gmail.com>
2024-09-29 12:49:07 -0300

    listo v2

--- components/ModalPedido.js
+++ components/ModalPedido.js
@@ -25,10 +25,6 @@ const ModalPedido = ({ handleClose, show, pedido }) => {
     return `${segmento1} ${segmento2} ${segmento3}`;
   };
 
-  console.log('pedido', pedido);
-  console.log('promo efectivo', promoEfectivo);
-
-
 
   const conDescuento = () => {
     return productos.some(p => p.categoria !== 'solo efectivo');
