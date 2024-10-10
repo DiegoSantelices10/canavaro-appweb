2eaa86abc55945365465702e781187efa3e663bc
DiegoSantelices10 <santelices.diego88@gmail.com>
2024-10-10 17:54:07 -0300

    ahora

--- components/Promotion.js
+++ components/Promotion.js
@@ -104,11 +104,9 @@ export default function Promotion({
     return bebidas?.find(item => item._id === _id);
   };
 
-  console.log('producst', products);
-
-
-  const res = products.filter(item => item.nombre === "Combo 1");
+  const res = products.filter(item => item.categoria === "promociones");
   console.log('res', res);
+
   return (
     <div className="mt-4">
       {(nombre === "Combo 4" || nombre === "Combo 5") && (
@@ -123,7 +121,7 @@ export default function Promotion({
               checked={select === "Combo 1"}
             />
             {products
-              ?.filter(item => item.nombre === "Combo 1")
+              ?.filter(item => item.nombre.includes("Combo 1"))
               .map(producto => {
                 return (
                   <div key={producto._id}>
@@ -143,7 +141,7 @@ export default function Promotion({
               checked={select === "Combo 2"}
             />
             {products
-              ?.filter(item => item.nombre === "Combo 2")
+              ?.filter(item => item.nombre.includes("Combo 2"))
               .map(producto => {
                 return (
                   <div key={producto._id}>
