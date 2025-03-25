b7394c5015ca0d8bd29be419bcd038ebbabe4796
Diego Santelices <dsantelices@midasconsultores.com.ar>
2025-03-25 08:45:40 -0300

    cambio socket

--- socket.js
+++ socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'https://pizzacanavaro-socket.com';
+const URL = 'https://pizza-canavaro.online';
 
 export const socket = io(URL);
