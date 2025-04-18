67b4264cdc5494a33edf3afd34fab908b88a459b
Diego Santelices <dsantelices@midasconsultores.com.ar>
2025-04-18 11:50:16 -0300

    a ver

--- components/CardPromotion.js
+++ components/CardPromotion.js
@@ -5,17 +5,26 @@ import Link from "next/link";
 export default function CardPromotion({ data: { nombre, descripcion, imagen } }) {
   return (
     <div>
-      <div className="w-64">
+      <div className="w-64 relative h-36 overflow-hidden rounded-lg">
+        {/* Fondo blureado */}
+        <img
+          src={imagen?.url || "/images/sin-imagen-center.png"}
+          alt={`fondo-${nombre}`}
+          className="absolute top-0 left-0 w-full h-full object-cover filter blur-md scale-110 z-0"
+        />
+
+        {/* Imagen principal */}
         <Link href={`/order/products/${convertToPath(nombre)}`}>
-          <a className="font-bold text-sm text-gray-800">
+          <a className="relative z-10 flex items-center justify-center w-full h-full">
             <img
               src={imagen?.url || "/images/sin-imagen-center.png"}
-              className="rounded-lg object-contain w-full h-40"
+              className="object-contain max-h-full"
               alt={nombre}
             />
           </a>
         </Link>
       </div>
+
       <div className="pt-2 pb-1 px-1 w-full flex flex-col flex-wrap">
         <Link href={`/order/products/${convertToPath(nombre)}`}>
           <a className="font-semibold text-sm font-montserrat text-neutral-800 ">{nombre.length > 65 ? nombre.substring(0, 65) + "..." : nombre}</a>
